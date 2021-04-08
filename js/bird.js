4cf73c82dadae5612fb3bad54d2b5919cc45bfff
Diego Barros <diego.fariabarros@gmail.com>
2021-04-08 18:41:11 -0300

    bird class

--- js/bird.js
+++ js/bird.js
@@ -1,3 +1,30 @@
 class Bird {
-    
+
+    constructor(skySelector) {
+
+        this.skyElement = document.querySelector(skySelector);
+
+        this.skyWidth = this.skyElement.clientWidth;
+        this.skyHeight = this.skyElement.clientHeight;
+
+        this.bird = document.createElement("div");
+        this.bird.setAttribute("id", "bird-");
+        this.bird.setAttribute("class", "bird");
+
+        this.skyElement.appendChild(this.bird);
+
+        this.x = this.skyWidth / 4;
+        this.y = this.skyHeight / 2;
+
+        this.bird.style.left = this.x + "px";
+        this.bird.style.top = this.y + "px";
+
+        this.speed = 0;
+    }
+
+
+
+
+
+
 }
---
+++ js/main.js
@@ -0,0 +1,8 @@
+window.onload = (event) => {
+
+    let bird = new Bird(".sky");
+
+
+    console.log("oi");
+
+};
